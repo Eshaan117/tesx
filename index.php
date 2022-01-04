45cc95b80f8f85f716b5a4acc11490a1fdc1a0d8
Eshaan117 <82329655+Eshaan117@users.noreply.github.com>
2022-01-04 10:30:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel = "stylesheet" href = "SignUp.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration System using PHP(Responsive)</title>
+
+
+</head>
+<body>
+             <!-- Now we are going to make a new website in here -->
+
+             <nav class = "Navs">
+
+                  <a href = "#" class = "alinks">Home</a>
+                  <a href = "#" class = "alinks">News</a>
+                  <a href = "#" class = "alinks">About</a>
+                  <a href = "#" class = "alinks">Links</a>
+
+
+
+
+             </nav>
+
+             <h2 class = "header" >SignUp</h2>
+
+             <form class = "Former">
+
+
+            <div class = "underdiv"> 
+                   <label>Username</label><br/>
+                   <input type = "text" name = "username"/>
+
+            </div>
+
+            <div class = "underdiv"> 
+                   <label>Password</label><br/>
+                   <input type = "password" name = "password"/>
+
+            </div>
+
+            <div class = "underdiv"> 
+                   <label>Confirm Password</label><br/>
+                   <input  type = "password" name = "cpassword"/>
+
+            </div>
+
+            <div class = "underdiv">
+
+                  <input type = "submit" id = "hadzel"/>
+
+
+            </div>
+
+
+
+
+             </form>
+
+
+             <div class = "fotter">
+
+
+             <ul>
+
+                       <li>Our Team</li>
+                       <li>Interns</li>
+                       <li>Careers</li>
+                       <li>Internship</li>
+                      
+
+
+
+             </ul>
+
+             
+             <ul>
+
+                       <li>Support</li>
+                       <li>Share</li>
+                       <li>Community</li>
+                     
+                     
+
+
+
+             </ul>
+
+
+             
+             <ul>
+
+                       <li>Download Apps</li>
+                       <li>Android App</li>
+                       <li>iOS App</li>
+                    
+
+
+
+             </ul>
+
+
+             </div>
+
+             <hr class = "hr"/>
+
+             <div id = "Fond">
+
+                    
+             <ul>
+                  
+                       <li><a href = "#">&copy;2022Assbook.com</a></li>
+                       <li><a href = "#">Terms of use</a></li>
+                       <li><a href = "#">Privacy</a></li>
+                       <li><a href = "#">Cookie Notice</li>
+
+
+             </ul>
+
+
+                
+
+
+                  
+
+
+
+
+             </div>
+
+
+
+
+
+
+
+    
+</body>
+</html>
